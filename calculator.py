8240ca1746ab77ee762f66f723bc7ca610007bec
Stanislav ZLP <stanislav.gao@gmail.com>
2020-09-14 17:26:20 +0300

    Calculator

---
+++ calculator.py
@@ -0,0 +1,21 @@
+# Вводные данные
+A = int(input())
+B = input()
+C = int(input())
+
+# Возможные базовые операции
+if B == '-':
+    print(A-C)
+elif B == '+':
+    print(A+C)
+elif B == '%':
+    print(A%B)
+elif B == '//':
+    print(A//B)
+elif B == '*':
+    print(A*B)
+elif B == '**':
+    print(A**B)
+elif B == '/':
+    print(A/B)
+
