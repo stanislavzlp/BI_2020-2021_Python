a6be3aa48f45e9e3c383c0f399ccd1eef8e42d8f
Me <Stanislav.gao@gmail.com>
2021-02-27 19:02:53 +0300

    fix

--- translator.py
+++ translator.py
@@ -1,7 +1,7 @@
 from Bio import SeqIO
 
 
-def translation(path_to_fasta, table_for_translation = "Standard"):
+def translation(path_to_fasta, table_for_translation="Standard"):
     with open(path_to_fasta, 'r') as fasta_file:
         for record in SeqIO.parse(fasta_file, 'fasta'):
             coding_dna = record.seq
